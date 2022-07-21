20c36d90faf438e36f0526de14b1bf97cef02c4f
Crab-Prog <ophelie.coudert@gmail.com>
2022-07-21 09:50:35 +0200

    teachers page with pictures en description of them

--- frontend/src/pages/Teachers.jsx
+++ frontend/src/pages/Teachers.jsx
@@ -10,26 +10,58 @@ import "@styles/Teachers.scss";
 
 function Teachers() {
   return (
-    <section id="teachers-gallery">
-      <div className="teacher-hover">
-        <p className="see-hover">Eve professeur de streetlady et pilate</p>
-        <img className="" src={djamel} alt="" />
-      </div>
-      <div className="teacher-hover">
-        <p className="see-hover">Eve professeur de streetlady et pilate</p>
-        <img className="" src={eve} alt="" />
-      </div>
-      <div className="teacher-hover">
-        <p className="see-hover">Eve professeur de streetlady et pilate</p>
-        <img className="" src={deem} alt="" />
-      </div>
-      <div className="teacher-hover">
-        <p className="see-hover">Eve professeur de streetlady et pilate</p>
-        <img className="" src={sophie} alt="" />
-      </div>
-      <div className="teacher-hover">
-        <p className="see-hover">Eve professeur de streetlady et pilate</p>
-        <img className="" src={mariel} alt="" />
+    <section className="flex flex-col items-center">
+      <h1 className="text-8xl text-white pb-8">Intervenants</h1>
+      <div id="teachers-gallery">
+        <div className="teacher-hover">
+          <div className="see-hover">
+            <p>
+              Djamel professeur de danse polyvalent, Dj et beatmaker. Il
+              pratique depuis plus de 20ans et à de l'expérience en matière
+              d'événementiel et de show.
+            </p>
+          </div>
+          <img className="" src={djamel} alt="" />
+        </div>
+        <div className="teacher-hover">
+          <div className="see-hover">
+            <p>
+              Eve professeure de Streetlady et Pilate : Elle a une personnalitée
+              explosive ! Toujours à l'écoute elle crée des chorégraphies
+              originales. Laissez parler la femme qui est en vous!
+            </p>
+          </div>
+          <img className="" src={eve} alt="" />
+        </div>
+        <div className="teacher-hover">
+          <div className="see-hover">
+            <p>
+              Deem professeur de BreakDance et amoureux des rythmes, sa passion
+              se transmet à travers une étude de la musique pour toujours aller
+              plus loins dans le mouvement.
+            </p>
+          </div>
+          <img className="" src={deem} alt="" />
+        </div>
+        <div className="teacher-hover">
+          <div className="see-hover">
+            <p>
+              Sophie & Hervé professeurs de Kizomba. Ils garantissent de la
+              bonne humeur, sont patients et impliqués! Parfait pour apprendre
+              une danse de couple!
+            </p>
+          </div>
+          <img className="" src={sophie} alt="" />
+        </div>
+        <div className="teacher-hover">
+          <div className="see-hover">
+            <p>
+              Mariel professeur de House dance et Locking, soyez prêt à
+              transpirer parce qu'il vous mènera au bout de vos forces !{" "}
+            </p>
+          </div>
+          <img className="" src={mariel} alt="" />
+        </div>
       </div>
     </section>
   );
